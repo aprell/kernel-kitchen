4e11d4a1bab472355a1c1af49e5452851237f65a
Andreas Prell <andreas.h.prell@gmail.com>
2025-04-10 09:04:08 +0200

    Add example: reverse array

---
+++ examples/cuda/reverse.cu
@@ -0,0 +1,66 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "vgpu.h"
+
+__global__ void reverse(float *a, int n) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    // Grid-stride loop
+    for (int i = idx; i < n/2; i += stride) {
+        float t = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = t;
+    }
+}
+
+int main(void) {
+    float *a, *d_a;
+    int n = 1 << 10;
+
+    a = (float *)malloc(n * sizeof(float));
+    assert(a);
+
+    CHECK(cudaMalloc((void **)&d_a, n * sizeof(float)));
+    assert(d_a);
+
+    for (int i = 0; i < n; i++) {
+        a[i] = i + 1;
+    }
+
+    CHECK(cudaMemcpy(d_a, a, n * sizeof(float), cudaMemcpyHostToDevice));
+
+    reverse<<<dim3(8), dim3(4)>>>(d_a, n);
+
+    CHECK(cudaMemcpy(a, d_a, n * sizeof(float), cudaMemcpyDeviceToHost));
+
+    for (int i = 0; i < 5; i++) {
+        printf("a[%d] = %.0f\n", i, a[i]);
+    }
+
+    puts("...");
+
+    for (int i = n - 5; i < n; i++) {
+        printf("a[%d] = %.0f\n", i, a[i]);
+    }
+
+    CHECK(cudaFree(d_a));
+
+    free(a);
+
+    return 0;
+}
+
+// CHECK: a[0] = 1024
+// CHECK: a[1] = 1023
+// CHECK: a[2] = 1022
+// CHECK: a[3] = 1021
+// CHECK: a[4] = 1020
+// ...
+// CHECK: a[1019] = 5
+// CHECK: a[1020] = 4
+// CHECK: a[1021] = 3
+// CHECK: a[1022] = 2
+// CHECK: a[1023] = 1
+
